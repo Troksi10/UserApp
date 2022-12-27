f764f1dd83d941384862163de7fedabeacf85b16
Ilija Trokovski <ilija.trokovski@gmail.com>
2022-12-27 02:36:11 +0100

    Users

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredUserAge, setEnteredUserAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+
+    const usernameChangeHandler = (event) => {
+      setEnteredUserName(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+      setEnteredUserAge(event.target.value);
+    };
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">UserName</label>
+        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
